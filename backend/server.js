0daddd719ec5d7cf7cab5ce29ec1b1a7719ef357
dave <mrdavidsaguev@proton.me>
2023-07-22 18:33:24 +0300

    add parsed nested data to a file upload response object

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,14 @@ app.get("/", (req, res) => {
 
 app.post("/upload_file", upload.array("filename"), uploadFiles);
 
+// let nestedData = {};
+// const returnParsedXMLNestedData = async (incomingNestedData) => {
+//   await incomingNestedData;
+//   setTimeout(async () => {
+//     return nestedData = incomingNestedData;
+//   }, 100);
+// };
+
 async function uploadFiles(req, res) {
   const files = req.files;
 
@@ -31,18 +39,48 @@ async function uploadFiles(req, res) {
   const parser = new xml2js.Parser({explicitArray : false});
   const data = await fs.promises.readFile(filePath);
 
+  // if your XML looks like this:
+  //     <?xml version="1.0" encoding="utf-8"?>
+  //     <invoice importance="high" logged="true">
+  //       <recipientName>DarkNetStore</recipientName>
+  //       <date>2022/05/14 16:21:00</date>
+  //       <toAccount>40817826200005536362</toAccount>
+  //       <amount>10000</amount>
+  //       <comment>Bitcoin payment</comment>
+  //     </invoice>
+
+  // then you can parse it & show its data by keys like this:
+  // parser.parseString(data, async function (err, result) {
+  //   const thisInvoiceData = {
+  //     amount: result.invoice.amount,
+  //     comment: result.invoice.comment,
+  //     date: result.invoice.date,
+  //     recipientName: result.invoice.recipientName,
+  //     toAccount: result.invoice.toAccount
+  //   };
+
+  //   return new Promise(async (resolve, reject) => {
+  //     resolve(await returnParsedXMLNestedData(JSON.parse(JSON.stringify(thisInvoiceData))));
+  //   });
+  // });
+
   const parsedData = parser.parseString(data, async function (err, result) {
-    return await result;
+    setTimeout(async() => {
+      return await result;
+    }, 1000);
   });
-  
-  return res.json({
-    path: filePath,
-    filename: fileName,
-    size: fileSize,
-    mimdetype: fileMimeType,
-    fileData: parsedData
-  });
-}
+
+  setTimeout(() => {
+    return res.json({
+      // nestedData: nestedData,
+      path: filePath,
+      filename: fileName,
+      size: fileSize,
+      mimdetype: fileMimeType,
+      fileData: parsedData
+    });
+  }, 3000);
+};
  
 app.listen(port, () => {
   console.log(`Success! Your application is running on port ${port}.`);
